4595365945223277b1acf26964f2da8503e692b2
422404 <elyanpoujol@gmail.com>
2021-06-03 11:42:18 +0200

    Bump version to 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Elyan Poujol (422404)'
 author = 'Elyan Poujol (422404)'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
